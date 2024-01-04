b2abe323a9088e4d391c076e42595dbc32ffda03
orthia194 <qscxza1231@naver.com>
2024-01-04 07:46:04 +0000

    orthia

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './AuthProvider';
+import { AuthProvider } from './authprovider';
 import Omok from './omok';
 import SignUp from './SignUp';
 import Login from './login';
